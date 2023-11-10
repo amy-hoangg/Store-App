35e8b11eb233afa4a33280cd90353e9361be9b13
Amy Hoang <my.hoang@tuni.fi>
2023-11-10 14:57:19 +0200

    test suites

---
+++ test/own/mvc.test.js
@@ -0,0 +1,38 @@
+//describe: test suite
+//it: test case
+describe('User Registration', () => {
+    // Test cases for user registration
+    it('create user log-in page', () => {
+        throw new Error('Not implemented');
+    });
+  
+    it('implement the frontpage', () => {
+        throw new Error('Not implemented');
+    });
+
+    it('create profile page', () => {
+        throw new Error('not implement')
+    }) 
+    it('create a setting page', () => {
+        throw new Error('not implement')
+    }) 
+    it('create wish list page', () => {
+        throw new Error('not implement')
+    }) 
+    it('implement order processing', () => {
+        throw new Error('not implement')
+    }) 
+    it('implement search and filtering', () => {
+        throw new Error('not implement')
+    }) 
+    it('shopping cart management', () => {
+        throw new Error('not implement')
+    }) 
+    it('log in with google', () => {
+        throw new Error('not implement')
+    }) 
+    it('user authentication', () => {
+        throw new Error('not implement')
+    }) 
+});
+  
