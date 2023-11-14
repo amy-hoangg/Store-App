e374b424981e51c0d708091215d8265a6e46cdb6
Minh Khanh Pham <minhkhanh.pham@tuni.fi>
2023-11-15 01:13:16 +0200

    10.4 v1

--- controllers/products.js
+++ controllers/products.js
@@ -1,11 +1,29 @@
+const responseUtils = require('./utils/responseUtils');
+const { getCurrentUser } = require("./auth/auth");
+const products = require("./products.json");
+
 /**
  * Send all products as JSON
  *
  * @param {http.ServerResponse} response
  */
-const getAllProducts = async response => {
+const getAllProducts = async (request, response) => {
   // TODO: 10.2 Implement this
-  throw new Error('Not Implemented');
+  // throw new Error('Not Implemented');
+  const authorizationHeader = request.headers.authorization;
+    const currentUser = await getCurrentUser(request);
+
+    if (!authorizationHeader || !currentUser) {
+      // Send Basic Authentication challenge
+      return responseUtils.basicAuthChallenge(response);
+    }
+
+    // Check if the user has either admin or customer role
+    if (currentUser.role !== 'admin' && currentUser.role !== 'customer') {
+      return responseUtils.forbidden(response);
+    }
+
+    return responseUtils.sendJson(response, products, 200);
 };
 
 module.exports = { getAllProducts };
