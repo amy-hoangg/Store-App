9467daf38848e6c287f7e4c7ff81fda8b1947f0a
Minh Khanh Pham <minhkhanh.pham@tuni.fi>
2023-11-16 18:31:55 +0200

    10.4 v2

--- controllers/products.js
+++ controllers/products.js
@@ -1,29 +1,16 @@
-const responseUtils = require('./utils/responseUtils');
-const { getCurrentUser } = require("./auth/auth");
-const products = require("./products.json");
+const responseUtils = require('../utils/responseUtils');
+const products = require("../products.json");
+
 
 /**
  * Send all products as JSON
  *
- * @param {http.ServerResponse} response
+ * @typedef {import('http').ServerResponse} ServerResponse
+ * @param {ServerResponse} response the http response
  */
-const getAllProducts = async (request, response) => {
-  // TODO: 10.2 Implement this
-  // throw new Error('Not Implemented');
-  const authorizationHeader = request.headers.authorization;
-    const currentUser = await getCurrentUser(request);
-
-    if (!authorizationHeader || !currentUser) {
-      // Send Basic Authentication challenge
-      return responseUtils.basicAuthChallenge(response);
-    }
-
-    // Check if the user has either admin or customer role
-    if (currentUser.role !== 'admin' && currentUser.role !== 'customer') {
-      return responseUtils.forbidden(response);
-    }
-
-    return responseUtils.sendJson(response, products, 200);
+// eslint-disable-next-line require-await
+const getAllProducts = async response => {
+  return responseUtils.sendJson(response, products, 200);
 };
 
 module.exports = { getAllProducts };
