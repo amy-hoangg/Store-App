0b9d67e25213462163bb0fbcdf58026afcfd749a
Minh Khanh Pham <minhkhanh.pham@tuni.fi>
2023-12-03 03:46:06 +0200

    v5 fix models/order.js

--- models/order.js
+++ models/order.js
@@ -1,48 +1,61 @@
 const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
 
-const orderItemSchema = new mongoose.Schema({
-  product: {
-    _id: {
-      type: mongoose.Schema.Types.ObjectId,
-      required: true
-    },
-    name: {
-      type: String,
-      required: true
-    },
-    price: {
-      type: Number,
-      required: true,
-      min: 0
-    },
-    description: {
-      type: String,
-      required: true
-    }
+const SCHEMA_DEFAULTS = {
+  name: {
+    minLength: 1,
+    maxLength: 50
   },
-  quantity: {
-    type: Number,
-    required: true,
-    min: 1 // Quantity should be a minimum of 1
-  }
-});
+};
 
-const orderSchema = new mongoose.Schema({
-  customerId: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: 'User',
+const orderSchema = new Schema({
+  _id: {
+    type: String,
     required: true
   },
-  items: {
-    type: [orderItemSchema],
-    required: true,
-    validate: {
-      validator: function (value) {
-        return value.length > 0; // Validate minimum length of items array
+  customerId: {
+    type: String,
+    required: true
+  },
+  items: [
+    {
+      _id: {
+        type: String,
+        required: true
       },
-      message: 'Order should have at least one item'
+      product: {
+        _id: {
+          type: String,
+          required: true
+        },
+        name: {
+          type: String,
+          required: true,
+          trim: true,
+          minLength: SCHEMA_DEFAULTS.name.minLength,
+          maxLength: SCHEMA_DEFAULTS.name.maxLength,
+        },
+        price: {
+          type: Number,
+          required: true,
+          validate: {
+            validator: function (n) {
+              return n > 0;
+            },
+            message: 'price cannot be 0'
+          }
+        },
+        description: {
+          type: String
+        }
+      },
+      quantity: {
+        type: Number,
+        required: true,
+        min: 1
+      }
     }
-  }
+  ]
 });
 
 
