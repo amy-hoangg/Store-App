f1a085f78814c680bd45b54d823208ba142ac174
Minh Khanh Pham <minhkhanh.pham@tuni.fi>
2023-12-03 04:26:27 +0200

    v7 fix models/order.js

--- models/order.js
+++ models/order.js
@@ -1,75 +1,58 @@
 const mongoose = require('mongoose');
-const Schema = mongoose.Schema;
 
-const SCHEMA_DEFAULTS = {
-  name: {
-    minLength: 1,
-    maxLength: 50
+const orderItemSchema = new mongoose.Schema({
+  product: {
+    _id: {
+      type: mongoose.Schema.Types.ObjectId,
+      required: true,
+    },
+    name: {
+      type: String,
+      required: true,
+    },
+    price: {
+      type: Number,
+      required: true,
+      min: 0,
+    },
+    description: {
+      type: String,
+      required: true,
+    },
   },
-};
+  quantity: {
+    type: Number,
+    required: true,
+    min: 1,
+  },
+});
 
-const orderSchema = new Schema({
+const orderSchema = new mongoose.Schema({
   _id: {
     type: mongoose.Schema.Types.ObjectId,
-    required: true
+    required: true,
   },
   customerId: {
     type: mongoose.Schema.Types.ObjectId,
     ref: 'User',
-    required: true
+    required: true,
   },
-  items: {
-    type: [
-      {
-        _id: {
-          type: mongoose.Schema.Types.ObjectId,
-          required: true,
-        },
-        product: {
-          _id: {
-            type: mongoose.Schema.Types.ObjectId,
-            ref: 'Product',
-            required: true,
-          },
-          name: {
-            type: String,
-            required: true,
-            trim: true,
-            minLength: SCHEMA_DEFAULTS.name.minLength,
-            maxLength: SCHEMA_DEFAULTS.name.maxLength,
-          },
-          price: {
-            type: Number,
-            required: true,
-            validate: {
-              validator: function (n) {
-                return n > 0;
-              },
-              message: 'Price cannot be 0',
-            },
-          },
-          description: {
-            type: String,
-          },
-        },
-        quantity: {
-          type: Number,
-          required: true,
-          min: 1,
-        },
-      },
-    ],
-    require: true,
-    validate: {
-      validator: function (value) {
-        return value.length > 0; // Validate minimum length of items array
-      },
-      message: 'Order should have at least one item',
-    },
+  items: [orderItemSchema],
+});
+
+orderSchema.set('toJSON', {
+  virtuals: false,
+  versionKey: false,
+  transform: function (doc, ret) {
+    ret.items = ret.items.map(item => ({
+      _id: item._id,
+      product: item.product,
+      quantity: item.quantity,
+    }));
+    return ret;
   },
 });
 
-
-orderSchema.set('toJSON', { virtuals: false, versionKey: false });
 const Order = mongoose.model('Order', orderSchema);
 module.exports = Order;
+
